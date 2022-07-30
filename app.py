a35fb68b0c93f7aca114916a9dd3c6e23cec96be
mtm145 <mateusmoresco1400@gmail.com>
2022-07-30 19:19:16 -0300

    Adicionando comentários

--- app.py
+++ app.py
@@ -4,26 +4,35 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
+
+# definindo servidor do Chrome
 service = Service("F:\chromedriver\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+# Url do web Scraping
 driver.get("https://www.nfl.com/stats/player-stats/")
 time.sleep(1)
+
+# localização da tag no html da página
 driver.find_element(
     By.XPATH, "//thead//tr//th//a[contains(text(),'TD')]").click()
 element = driver.find_element(
     By.XPATH, "//tbody"
 )
+
+# transformando os dados em HTML legível
 dadosHtml = element.get_attribute('outerHTML')
 soup = BeautifulSoup(dadosHtml, 'html.parser')
 
 
+# localizando as tags necessárias
 listData = soup.find(name='tbody')
 lineData = listData.find_all(name='td')
 columnData = listData.find_all(name='tr')
 
-top10players = []
 
+# formatação dos dados
+top10players = []
 for c in range(0, 150, 16):
     top10players.append(lineData[c].text)
 
